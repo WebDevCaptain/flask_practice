cdca6926b19758ddc64e5c843489666a4eab11ae
Shreyash <shreyashkumar95@gmail.com>
2020-02-01 11:44:10 +0530

    Completed the Part1 of Brad's Flask Workshop

---
+++ data.py
@@ -0,0 +1,23 @@
+def Articles():
+    articles = [{
+        'id': 1,
+        'title': 'Flask is awesome',
+        'body':
+        'Flask is a micro web-framework which is very popular among Startups and Unicorns',
+        'author': 'Shreyash',
+        'createdAt': '01-02-2020'
+    }, {
+        'id': 2,
+        'title': 'Node is my favourite technology',
+        'body': 'NodeJS is very popular tech in todays Web-developer land',
+        'author': 'CyberArmy',
+        'createdAt': '01-02-2020'
+    }, {
+        'id': 3,
+        'title': 'Django is also great',
+        'body':
+        'Django is an MVC web-framework which is very popular among Startups and Unicorns',
+        'author': 'Shreyash',
+        'createdAt': '01-02-2020'
+    }]
+    return articles
